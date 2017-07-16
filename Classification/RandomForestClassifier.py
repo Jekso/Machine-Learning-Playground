243ab9b4d32a90fa762c5add50f16431b8b551c9
Jekso Senpai <jeksogsa@gmail.com>
2017-07-16 13:53:51 +0300

    IrisProblem Solved using RandomForestClassifier

--- Classification/RandomForestClassifier.py
+++ Classification/RandomForestClassifier.py
@@ -1,9 +1,9 @@
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 x = np.array([[100, 150], [3, 2], [10, 25], [200, 200], [15, 10]])
 y = np.array([2, 1, 1, 2, 1])
 
-model = DecisionTreeClassifier()
+model = RandomForestClassifier(n_estimators=100)
 model.fit(x, y)
 print model.predict(np.array([[55, 55]]))
