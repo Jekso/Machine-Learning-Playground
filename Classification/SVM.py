1640af3efbdde38ee55d73ae32baa4fd5453b815
Jekso Senpai <jeksogsa@gmail.com>
2017-07-16 09:32:25 +0300

    add some comments

--- Classification/SVM.py
+++ Classification/SVM.py
@@ -4,6 +4,7 @@ from sklearn import svm
 x = np.array([[100, 150], [3, 2], [10, 25], [200, 200], [15, 10]])
 y = np.array([2, 1, 1, 2, 1])
 
+# svc stands for support vector classifier
 model = svm.SVC()
 model.fit(x, y)
 print model.predict(np.array([[500, 70]]))
