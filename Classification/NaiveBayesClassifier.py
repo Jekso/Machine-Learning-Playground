4d81a742d6fef6a21c2c2cbec71de98fe9a6ffa9
Jekso Senpai <jeksogsa@gmail.com>
2017-07-16 09:01:11 +0300

    NaibeBayes (GaussianNB) Classifier Complete

--- Classification/NaiveBayesClassifier.py
+++ Classification/NaiveBayesClassifier.py
@@ -1,9 +1,10 @@
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 
 x = np.array([[100, 150], [3, 2], [10, 25], [200, 200], [15, 10]])
 y = np.array([2, 1, 1, 2, 1])
 
-model = KNeighborsClassifier(3)
+model = GaussianNB()
 model.fit(x, y)
 print model.predict(np.array([[500, 70]]))
+
