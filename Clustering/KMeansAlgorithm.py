ade35924c4158b5e655c49465f1c2997c64a15c3
Jekso Senpai <jeksogsa@gmail.com>
2017-07-19 04:04:49 +0300

    Started solving Jupyter problems - Python Crash Course

---
+++ Clustering/KMeansAlgorithm.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+
+x, y = make_blobs()
+
+
+model = KMeans(6)
+model.fit(x, y)
+y_predicted = model.predict(x)
