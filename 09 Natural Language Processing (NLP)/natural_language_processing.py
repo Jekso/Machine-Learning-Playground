e40f56482515c3a84d3e9eee24af08dd2dbc879d
Jekso.Senpai <jeksogsa@gmail.com>
2017-09-01 03:31:06 +0200

    NLP reviews problem modify deprecated cross_validation library

--- 09 Natural Language Processing (NLP)/natural_language_processing.py
+++ 09 Natural Language Processing (NLP)/natural_language_processing.py
@@ -31,7 +31,7 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Fitting Naive Bayes to the Training set
